40cc319107b02f2c86d6337a9338cb5bba7d6f82
alex <alexylee415@gmail.com>
2023-05-15 14:30:42 -0700

    Update the flask application code

--- app.py
+++ app.py
@@ -3,4 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Simple Flask application \n This is an additional line'
+    print('Hello, Simple Flask application \n \
+          This is an additional line \n \
+          This is the third line')
